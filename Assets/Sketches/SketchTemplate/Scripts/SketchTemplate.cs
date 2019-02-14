329c4e11e3b7c524ca09929485e27d234b2c8561
u16kuma <u16.kuma@gmail.com>
2019-02-15 00:37:34 +0900

    SketchTemplateの作成

---
+++ Assets/Sketches/SketchTemplate/Scripts/SketchTemplate.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SketchTemplate : MonoBehaviour
+{
+	public ComputeShader computeShader;
+	
+	private void Start()
+	{
+		computeShader.Dispatch(0, 1, 1, 1);
+	}
+}
