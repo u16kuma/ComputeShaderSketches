f8b398a1d48abf739e5dfc35591e31a4a9fe359d
u16kuma <u16.kuma@gmail.com>
2019-02-24 15:35:35 +0900

    シーン分割

--- Assets/Sketches/Scripts/Sketches.cs
+++ Assets/Sketches/Scripts/Sketches.cs
@@ -4,4 +4,15 @@ using UnityEngine;
 
 public class Sketches : MonoBehaviour
 {
+	[SerializeField]
+	private List<Sketch> sketchPrefabList;
+
+	private void Start()
+	{
+		// とりあえずすべてインスタンス化
+		foreach (var sketchPrefab in sketchPrefabList)
+		{
+			var sketch = Instantiate(sketchPrefab);
+		}
+	}
 }
