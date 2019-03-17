be2722b1a102f80d45c331e39ae63be8883bf025
u16kuma <u16.kuma@gmail.com>
2019-03-17 18:29:50 +0900

    ビルド用スクリプトを追加

---
+++ Assets/Editor/BatchBuild.cs
@@ -0,0 +1,14 @@
+﻿#if UNITY_EDITOR
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class BatchBuild
+{
+	public static void Build()
+	{
+		EditorApplication.Exit(0);
+	}
+}
+#endif
